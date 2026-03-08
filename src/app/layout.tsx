f6d8db3fb1dc16868123f248601b39707b1fdb39
Matthew <mat.loughnane@gmail.com>
2026-03-08 18:57:47 +0000

    Updated with .ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,9 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-    title: "Bridgit Dashboard",
-    description: "Bridgit non-API dashboard"
+    title: "Bridgit Mock Platform Dashboard",
+    description: "Mock Bridgit Dashboard",
+    icons: { icon: "/favicon.png" }
 };
 
 export default function RootLayout({
